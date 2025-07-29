e2fb088efd252a116782c007be119cf45bda5c39
Frank Ballmeyer <frank@ballmeyer.com>
2025-07-29 15:44:01 +0200

    Fixed configuration of provider column in tenant #30

--- src/Ballware.Meta.Data/Persistables/Tenant.cs
+++ src/Ballware.Meta.Data/Persistables/Tenant.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 using Ballware.Shared.Data.Persistables;
 
@@ -21,6 +22,8 @@ public class Tenant : IEntity, IAuditable
 
     public bool ManagedDatabase { get; set; }
 
+    [Required]
+    [MaxLength(20)]
     public string? Provider { get; set; }
 
     public string? Server { get; set; }
