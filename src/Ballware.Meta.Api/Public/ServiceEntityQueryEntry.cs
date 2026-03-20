467a5fec399008bfc924e39b9183c41877ee0ed5
Frank Ballmeyer <frank@ballmeyer.com>
2026-03-20 18:11:21 +0100

    Fixed missing json property name annotations #58

--- src/Ballware.Meta.Api/Public/ServiceEntityQueryEntry.cs
+++ src/Ballware.Meta.Api/Public/ServiceEntityQueryEntry.cs
@@ -29,20 +29,26 @@ public class ServiceEntityQueryResultColumn
 public class ServiceEntityQueryEntry
 {
     [JsonPropertyName("identifier")]
+    [Newtonsoft.Json.JsonProperty("identifier")]  
     public required string Identifier { get; set; }
 
     [JsonPropertyName("query")]
+    [Newtonsoft.Json.JsonProperty("query")]
     public required string Query { get; set; }
     
     [JsonPropertyName("description")]
+    [Newtonsoft.Json.JsonProperty("description")]   
     public string? Description { get; set; }
     
     [JsonPropertyName("ai_enabled")]
+    [Newtonsoft.Json.JsonProperty("ai_enabled")]
     public bool? AiEnabled { get; set; }
     
     [JsonPropertyName("parameters")]
+    [Newtonsoft.Json.JsonProperty("parameters")]
     public ServiceEntityQueryParameter[]? Parameters { get; set; }
     
     [JsonPropertyName("result_columns")]
+    [Newtonsoft.Json.JsonProperty("result_columns")]
     public ServiceEntityQueryResultColumn[]? ResultColumns { get; set; }
 }
